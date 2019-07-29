9aab1d7906af6c8fa2e932b859adebd98db39264
cnscj <cnscj@qq.com>
2019-07-29 22:00:23 +0800

    fix:EffectEditor+GOPool

--- STGGame/Assets/ResEditor/ShareRes/ResourceEditors/Effect/Scripts/PublicFxPlayEffect.cs
+++ STGGame/Assets/ResEditor/ShareRes/ResourceEditors/Effect/Scripts/PublicFxPlayEffect.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace STGGame
 {
     public class PublicFxPlayEffect : MonoBehaviour
     {
+        public GameObject effectBtnPrefab;
+        public GameObject effectList;
         private GameObject m_playNode;
         private List<GameObject> effectsList = new List<GameObject>();
         void Start()
@@ -30,7 +33,7 @@ namespace STGGame
             if (effectsNode)
             {
                 effectsList.Clear();
-                foreach (var effect in effectsNode.GetComponentsInChildren<Transform>(true))
+                foreach (var effect in effectsNode.GetComponentsInChildren<Transform>())
                 {
                     if (effect.gameObject == effectsNode)
                     {
@@ -41,9 +44,28 @@ namespace STGGame
                 }
 
                 //生成UI
+                float buttonHeight = (effectBtnPrefab.transform as RectTransform).rect.height;
+                int i = 0;
+                float contentHeight = effectsList.Count * buttonHeight;
                 foreach (var effect in effectsList)
                 {
+                    var go = Instantiate(effectBtnPrefab, effectList.transform);
+                    go.name = effect.name;
+                    go.SetActive(true);
 
+                    RectTransform rt = go.transform as RectTransform;
+                    rt.anchoredPosition = new Vector2(70, -70-(-i++ * buttonHeight + contentHeight / 2));
+
+
+                    Text text = go.GetComponentInChildren<Text>();
+                    text.text = effect.name;
+
+                    Button btn = go.GetComponent<Button>();
+                    btn.onClick.AddListener(() =>
+                    {
+                        var newFx = Instantiate(effect, m_playNode.transform);
+                        Destroy(newFx, 10.0f);
+                    });
                 }
             }
         }
