d5f1446520ec9384bf55ffa3b039a9ce7e126846
cnscj <cnscj@qq.com>
2019-08-12 21:58:02 +0800

    fix:配置转换

---
+++ STGGame/Assets/THFramework/Component/Excel2Unity/Scripts/Editor/BaseExcelConverter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace THEditor
+{
+    public class BaseExcelConverter
+    {
+        public virtual void Convert(string exportPath)
+        {
+            
+        }
+    }
+
+}
---
+++ STGGame/Assets/THFramework/Component/Excel2Unity/Scripts/Editor/Excel2Json.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace THEditor
+{
+    public class Excel2Json : BaseExcelConverter
+    {
+        public override void Convert(string exportPath)
+        {
+            
+        }
+    }
+
+}
