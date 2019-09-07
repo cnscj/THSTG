ea5dab141efd02a839b561e4a6b3f3886f077688
cnscj <cnscj@qq.com>
2019-09-07 15:56:16 +0800

    fix:增加CSVValue类型转换口

---
+++ STGGame/Assets/THFramework/Library/Component/ConfigReader/CsvReader/CSVValue.cs
@@ -0,0 +1,30 @@
+﻿
+// ------------------------------ //
+// Product Name : CSV_Read&Write
+// Company Name : MOESTONE
+// Author  Name : Eazey Wang
+// Create  Data : 2017/12/16
+// ------------------------------ //
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace THGame
+{
+    public class CSVValue
+    {
+        private string m_content;
+
+        public CSVValue(string content) { m_content = string.Format("{0}", content); }
+        public CSVValue(int content) { m_content = string.Format("{0}", content); }
+        public CSVValue(bool content) { m_content = string.Format("{0}", content); }
+        public CSVValue(double content) { m_content = string.Format("{0}", content); }
+
+        public override string ToString() { return m_content; }
+        public int ToInt() { return int.Parse(m_content); }
+        public bool ToBool() { return bool.Parse(m_content); }
+        public double ToDouble() { return double.Parse(m_content); }
+
+    }
+}
