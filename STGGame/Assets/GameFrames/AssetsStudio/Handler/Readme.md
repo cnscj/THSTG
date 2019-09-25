659f2cd628390889ca90dbbbbac86b5ea074a881
cnscj <cnscj@qq.com>
2019-09-25 21:46:34 +0800

    fix:.....

---
+++ STGGame/Assets/GameFrames/AssetsStudio/Handler/Readme.md
@@ -0,0 +1,8 @@
+# AssetStudio KIt 
+## Core介绍
+1. ResourceModule #资源工具
+2. ResourceProcesser #资源加工
+3. ResourceBuilder #资源打包
+4. ResourcenDownload #资源下载
+5. ResourcenUpdate #资源更新
+6. ResourcenLoader #资源加载
---
+++ STGGame/Assets/GameFrames/AssetsStudio/Modules/Config/Tools/Excel2Unity/Scripts/Editor/ExcelMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEditor.Animations;
+using UnityEngine;
+using XLibrary;
+
+namespace ASEditor
+{
+    public class ExcelMenu
+    {
+
+        
+
+    }
+}
