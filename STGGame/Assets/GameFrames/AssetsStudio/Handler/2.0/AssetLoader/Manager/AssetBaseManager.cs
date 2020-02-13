d6bb2e8e462d80b24be730179773a7d54db3db72
cnscj <cnscj@qq.com>
2020-02-13 23:12:43 +0800

    fix:音效管理器 0%/5%

--- STGGame/Assets/GameFrames/AssetsStudio/Handler/2.0/AssetLoader/Manager/AssetBaseManager.cs
+++ STGGame/Assets/GameFrames/AssetsStudio/Handler/2.0/AssetLoader/Manager/AssetBaseManager.cs
@@ -4,8 +4,38 @@ using XLibrary.Package;
 
 namespace ASGame
 {
+    /// <summary>
+    /// 一个简易对象池及轮询系统
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class AssetBaseManager<T> : MonoSingleton<T> where T: MonoBehaviour
     {
-        
+        [Header("定时清理缓存间隔(秒):")]
+        public float clearCacheDuration = 10f;
+
+        private float m_cacheTimeTemp;
+
+        private void Update()
+        {
+            OnUpdate();
+
+            if (clearCacheDuration < 0) return;
+            m_cacheTimeTemp += Time.deltaTime;
+            if (m_cacheTimeTemp >= clearCacheDuration)
+            {
+                OnClean();
+                m_cacheTimeTemp -= clearCacheDuration;
+            }
+        }
+
+        protected void OnUpdate()
+        {
+
+        }
+
+        protected void OnClean()
+        {
+
+        }
     }
 }
--- STGGame/Assets/GameFrames/AssetsStudio/Handler/2.0/AssetLoader/Manager/AssetTextureManager.cs
+++ STGGame/Assets/GameFrames/AssetsStudio/Handler/2.0/AssetLoader/Manager/AssetTextureManager.cs
@@ -1,10 +1,60 @@
 ﻿
+using System;
 using XLibrary.Package;
 
 namespace ASGame
 {
     public class AssetTextureManager : AssetBaseManager<AssetTextureManager>
     {
-        
+        public delegate void OnLoadSuccess(object texture);
+        public delegate void OnLoadFailed(string error);
+
+        public void Reset()
+        {
+
+        }
+
+        public void Revive()
+        {
+
+        }
+
+        public void Recycle()
+        {
+
+        }
+
+        public void OnLoadCompleted(string error)
+        {
+
+        }
+
+        public void ClearAll()
+        {
+
+        }
+
+        public void LoadTexture(string url, OnLoadSuccess onLoadSuccess, OnLoadFailed onLoadFailed, bool isAsyncLoading)
+        {
+
+        }
+
+        public void LoadUrl(string url, OnLoadSuccess onLoadSuccess, OnLoadFailed onLoadFailed, bool isAsyncLoading)
+        {
+
+        }
+
+        public void UnloadTexture(string url, string name)
+        {
+
+        }
+
+
+
+
+        void OnDestroy()
+        {
+            ClearAll();
+        }
     }
 }
