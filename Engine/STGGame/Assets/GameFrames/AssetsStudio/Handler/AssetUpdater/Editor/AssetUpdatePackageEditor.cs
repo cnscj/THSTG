41355cb0774c2db99033a61b1d7e8c6a0d028e75
cnscj <cnscj@qq.com>
2020-09-06 23:14:21 +0800

    fix:搜索框

--- Engine/STGGame/Assets/GameFrames/AssetsStudio/Handler/AssetUpdater/Editor/AssetUpdatePackageEditor.cs
+++ Engine/STGGame/Assets/GameFrames/AssetsStudio/Handler/AssetUpdater/Editor/AssetUpdatePackageEditor.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using XLibEditor;
 
@@ -7,7 +8,19 @@ namespace ASEditor
 {
     public class AssetUpdatePackageEditor : WindowGUI<AssetUpdatePackageEditor>
     {
-        
+        private SearchTextField searchTextField = new SearchTextField();
+
+        [MenuItem("AssetsStudio/资源工具/资源分包器")]
+        static void ShowWnd()
+        {
+            ShowWindow("资源分包配置");
+        }
+
+        //参考PackagesWindow
+        protected override void OnShow()
+        {
+            searchTextField.OnGUI();
+        }
     }
 
 }
---
+++ Engine/STGGame/Assets/GameFrames/XLibrary/Modules/InspectorGUI/Editor/Component/GUIComponent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace XLibEditor
+{
+    public class GUIComponent
+    {
+        public virtual void OnGUI()
+        {
+
+        }
+    }
+}
