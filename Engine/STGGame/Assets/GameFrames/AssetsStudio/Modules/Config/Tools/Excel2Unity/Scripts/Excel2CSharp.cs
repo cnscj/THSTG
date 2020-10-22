191d25b5993e87772f30e8968f5fa83ca7bb2ec6
cnscj <cnscj@qq.com>
2020-10-22 22:39:58 +0800

    fix:......

---
+++ Engine/STGGame/Assets/GameFrames/AssetsStudio/Modules/Config/Tools/Excel2Unity/Scripts/Excel2CSharp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Text;
+
+namespace ASGame
+{
+    //TODO:
+    public class Excel2CSharp : BaseExcelConverter
+    {
+        public static string CONTENT_TEMPLATE = @"
+            public class $sheetName$
+            {
+                $sheetTypes$
+            };
+        ";
+
+        public Excel2CSharp(string excelFile) : base(excelFile)
+        {
+
+        }
+
+        public override string OnConvert(DataSet dataSet)
+        {
+            return "";
+
+            
+        }
+    }
+
+}
--- Engine/STGGame/Assets/GameFrames/THFramework/ACT/SkillSystem/Core/SkillManager.cs
+++ Engine/STGGame/Assets/GameFrames/THFramework/ACT/SkillSystem/Core/SkillManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XLibrary.Collection;
 using XLibrary.Package;
 using Object = UnityEngine.Object;
 
@@ -87,14 +88,19 @@ namespace THGame
     public class SkillScheduler
     {
         //采用时间片轮转的方式
-        public class Job
+        public class Job : IComparable
         {
-
+            public float time;
+            public int CompareTo(object obj)
+            {
+                Job job = obj as Job;
+                return time.CompareTo(job);
+            }
         }
 
         public Action onExecute;
         public Action onFinish;
-        //一个按时间优先度排序的队列,每次检测头部
+        private PriorityQueue<Job> _scheduleJobs;//一个按时间优先度排序的队列,每次检测头部
     }
 
     //触发器
