a79a1a425930ab9ae24a1eca023f0228b43fc711
cnscj <cnscj@qq.com>
2021-04-09 21:28:57 +0800

    fix:lua资源加载器

--- Game/Script/Client/Base/Library/AssetManager/AssetManager.lua
+++ Game/Script/Client/Base/Library/AssetManager/AssetManager.lua
@@ -3,14 +3,43 @@ local AssetLoaderManager = CSharp.AssetLoaderManager
 
 function M:ctor()
 
+
 end
 
 function M:loadAssetBundle(abPath,onSuccess,onFailed)
+    onSuccess = self:_createSuccessCallback(onSuccess)
+    onFailed = self:_createFailedCallback(onFailed)
     
 end
 
 function M:loadAssetBundleResource(abPath,resPath,onSuccess,onFailed)
-    
+    onSuccess = self:_createSuccessCallback(onSuccess)
+    onFailed = self:_createFailedCallback(onFailed)
+
+end
+
+function M:_createSuccessCallback(callback)
+    return function (...)
+        self:_onSuccess(callback, ...)
+    end
+end
+
+function M:_createFailedCallback(callback)
+    return function (...)
+        self:_onFailed(callback, ...)
+    end
+end
+
+function M:_onSuccess(callback, ... )
+
+
+    if callback then callback(...) end 
+end
+
+function M:_onFailed(callback, ... )
+
+
+    if callback then callback(...) end 
 end
 
 rawset(_G, "AssetManager", false)
