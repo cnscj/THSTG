972f5f5cc67afe3902ceb8f580902e9d7963c1a1
cnscj <cnscj@qq.com>
2021-12-25 17:30:51 +0800

    fix:更新CompType

--- Game/Script/Client/Base/Context/Types/CompType.lua
+++ Game/Script/Client/Base/Context/Types/CompType.lua
@@ -1,7 +1,23 @@
 local M = {
     ["Component"] = GComponent,
-    ["Button"] = GButton,
+    ["ScrollPane"] = GScrollPane,
     ["Label"] = GLabel,
+    ["RichText"] = GRichText,
+    ["Button"] = GButton,
+    ["Tag"] = GTag,
+    ["List"] = GList,
+    ["Checkbox"] = GCheckBox,
+    ["RadioButton"] = GRadioButton,
+    ["TextInput"] = GTextInput,
+    ["Slider"] = GSlider,
+    ["SliderInt"] = GSliderInt,
+    ["ComboBox"] = GComboBox,
+    ["ProgressBar"] = GProgressBar,
+    ["Image"] = GImage,
+    ["ScrollView"] = GScrollView,
+    ["Loader"] = GLoader,
+    ["Graph"] = GGraph,
+    ["Accordion"] = GAccordion,
 }
 
 
